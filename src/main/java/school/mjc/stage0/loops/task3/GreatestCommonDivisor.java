3e381dabf170d73b0bfcba297d55f6a1af3a82e4
Islombek Yadgarov <islam.yadgarov.677@gmail.com>
2022-11-07 18:44:23 +0500

    ""

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -1,6 +1,13 @@
 package school.mjc.stage0.loops.task3;
-
 public class GreatestCommonDivisor {
+    int a = 0;
     public void printGCD(int first, int second) {
+        for (int i = 1; i<=first && i<= second; i++){
+            if(first % i == 0 && second % i == 0){
+                a = i;
+            }
+
+        }
+        System.out.println(a);
     }
 }
